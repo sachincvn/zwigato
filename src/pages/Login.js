48191fc8254e426cf91d221cdd097d1a8ae8f2ce
Sachin Chavan <sachincvn962@gmail.com>
2023-05-01 22:16:26 +0530

    fixed error

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,35 +4,6 @@ import Footer from "../components/Footer";
 
 export const Login = () => {
 
-  const [credentials, setCredentials] = useState({
-    userEmail: "",
-    userPassword: "",
-  });
-
-  const handelSubmit = async (e) => {
-    e.preventDefault();
-
-    const response = await fetch("http://localhost:3001/api/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        password: credentials.userPassword,
-        email: credentials.userEmail,
-      }),
-    });
-
-    const json = await response.json();
-    console.log(json);
-    if (!json.success) {
-        alert("Enter valid credentials")
-    }
-  };
-
-  const onChange = (e) => {
-    setCredentials({ ...credentials, [e.target.name]: e.target.value });
-  };
   return (
     <>
       <Navbar />
@@ -45,7 +16,6 @@ export const Login = () => {
               class="form-control"
               id="userEmail"
               name="userEmail"
-              value={credentials.userEmail}
               aria-describedby="emailHelp"
               placeholder="Enter email"
             />
@@ -60,7 +30,6 @@ export const Login = () => {
               class="form-control"
               id="userPassword"
               name="userPassword"
-              value={userPassword}
               placeholder="Password"
             />
           </div>
